2b6e39250b59a264f4309a2e2735b4c00b1edf6d
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2018-10-04 13:58:51 -0400

    fix language default when a language status is off

--- boutique/includes/ClicShopping/Apps/Configuration/Langues/Classes/ClicShoppingAdmin/Status.php
+++ boutique/includes/ClicShopping/Apps/Configuration/Langues/Classes/ClicShoppingAdmin/Status.php
@@ -34,14 +34,35 @@
 
       if ($status == 1) {
         return $CLICSHOPPING_Db->save('languages', ['status' => 1],
-                                            ['languages_id' => (int)$languages_id]
+                                                   ['languages_id' => (int)$languages_id]
                               );
 
       } elseif ($status == 0) {
 
+        $Qcheck = $CLICSHOPPING_Db->prepare('select code
+                                             from :table_languages
+                                             where languages_id = :languages_id
+                                            ');
+        $Qcheck->bindValue(':languages_id', $languages_id);
+        $Qcheck->execute();
+
+        if ($Qcheck->value('code') == DEFAULT_LANGUAGE && $Qcheck->value('code') != 'en') {
+
+          $Qupdate = $CLICSHOPPING_Db->prepare('update :table_configuration
+                                                set configuration_value = :configuration_value
+                                                where configuration_key = :configuration_key
+                                               ');
+          $Qupdate->bindValue(':configuration_value', 'en');
+          $Qupdate->bindValue(':configuration_key', 'DEFAULT_LANGUAGE');
+
+          $Qupdate->execute();
+
+
+        }
+
         return $CLICSHOPPING_Db->save('languages', ['status' => 0],
-                                            ['languages_id' => (int)$languages_id]
-                              );
+                                                   ['languages_id' => (int)$languages_id]
+                                     );
 
       } else {
         return -1;
