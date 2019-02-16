47a47135f22485e02da44cd218e00e8be173f512
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-02-15 22:31:18 -0500

    remove some element from url

--- shop/includes/ClicShopping/Sites/Shop/RewriteUrl.php
+++ shop/includes/ClicShopping/Sites/Shop/RewriteUrl.php
@@ -29,9 +29,11 @@
  */
     protected function getSkipAccents( $str, $charset='utf-8' ) {
       $str = htmlentities( $str, ENT_NOQUOTES, $charset );
+
       $str = preg_replace( '#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str );
       $str = preg_replace( '#&([A-za-z]{2})(?:lig);#', '\1', $str );
       $str = preg_replace( '#&[^;]+;#', '', $str );
+      $str = preg_replace('/[^A-Za-z0-9\-]/', '', $str); // Removes special chars
 
       return $str;
     }
