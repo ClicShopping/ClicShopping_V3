668ab7dcf0d4e9404e5a8bdd0bbe8d8f479d1fe6
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-02-27 17:36:30 -0500

    remove not use

--- shop/sources/template/Default/modules/modules_products_info/content/products_info_tab_boostrap.php
+++
@@ -1,8 +0,0 @@
-<div class="col-md-<?php echo $content_width; ?>">
-  <div class="separator"></div>
-    <div class="contentText">
-      <?php echo $product_tab_title; ?>
-      <?php echo $product_tab_description; ?>
-    <div>
-  <div>
-<div>
