32bc8ecf525f9c6abe05c55ab769e3852d716030
ClicShopping <Yae8mr2o*>
2019-10-28 11:31:47 -0400

    version321 remove double --

--- shop/includes/ClicShopping/Sites/Shop/RewriteUrl.php
+++ shop/includes/ClicShopping/Sites/Shop/RewriteUrl.php
@@ -833,9 +833,6 @@
         $str = static::getRemoveAccents($str);
       }
 
-      $str = htmlentities($str, ENT_NOQUOTES, $charset);
-
-      $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
       $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str);
       $str = preg_replace('#&[^;]+;#', '', $str);
       $str = preg_replace('/[^A-Za-z0-9\-]/', '', $str); // Removes special chars
@@ -847,10 +844,12 @@
      * @param string $str
      * @return string
      */
-    protected function replaceString(string $str): string
+    protected function replaceString($str): string
     {
       $string = str_replace(' ', '-', $str);
       $string = $this->getSkipAccents($string);
+
+      $string = str_replace('--', '-', $string);
 
       return $string;
     }
