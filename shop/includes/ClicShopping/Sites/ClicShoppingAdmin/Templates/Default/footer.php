6b49db773b15d2566845f311883c94aa3e5c0aef
ClicShopping <Yae8mr2o*>
2019-11-12 17:42:02 -0500

    version 3.2111 revert

--- shop/includes/ClicShopping/Sites/ClicShoppingAdmin/Templates/Default/footer.php
+++ shop/includes/ClicShopping/Sites/ClicShoppingAdmin/Templates/Default/footer.php
@@ -19,19 +19,18 @@
 </div>
 <footer>
   <div id="footer">
-   <nav class="navbar navbar-fixed-bottom navbar-light bg-faded">
-     <div class="row footerCadre">
-     <div class="col-md-12 navbar-collapse text-md-left" id="footer-body">
-        <span class="col-md-3 navbar-text">
-          <?php echo 'ClicShopping™ - V. ' . CLICSHOPPING::getVersion(); ?> - &copy; 2008 - <?php echo date('Y'); ?><br/>
-        </span>
+    <div class="footerCadre">
+          <span class="navbar navbar-fixed-bottom navbar-light bg-faded">
+            <div class="col-md-12 navbar-collapse text-md-left" id="footer-body">
+              <span class="col-md-3 navbar-text">
+                <?php echo 'ClicShopping™ - V. ' . CLICSHOPPING::getVersion(); ?> - &copy; 2008 - <?php echo date('Y'); ?><br/>
+              </span>
 <?php
   if (isset($_SESSION['admin'])) {
     ?>
-       <span class="col-md-4 navbar-text nav-item text-md-center">
-          <?php echo $CLICSHOPPING_Language->getLanguageText(); ?>
-       </span>
-       <span class="col-md-4 navbar-text float-md-right text-md-center footerHelp">
+    <span
+      class="col-md-4 navbar-text nav-item text-md-center"><?php echo $CLICSHOPPING_Language->getLanguageText(); ?></span>
+    <span class="col-md-4 navbar-text float-md-right text-md-center footerHelp">
 <?php
   echo CLICSHOPPING::getDef('text_legend');
   echo HTML::image($CLICSHOPPING_Template->getImageDirectory() . 'icons/edit.gif', CLICSHOPPING::getDef('image_edit')) . ' ' . CLICSHOPPING::getDef('image_edit') . ' - ';
@@ -39,15 +38,16 @@
   echo HTML::image($CLICSHOPPING_Template->getImageDirectory() . 'icons/delete.gif', CLICSHOPPING::getDef('image_delete')) . ' ' . CLICSHOPPING::getDef('image_delete') . ' - ';
   echo HTML::image($CLICSHOPPING_Template->getImageDirectory() . 'icons/preview.gif', CLICSHOPPING::getDef('image_preview')) . ' ' . CLICSHOPPING::getDef('image_preview') . ' - ';
 ?>
-       <span class="col-md-1 navbar-text float-md-right text-md-right go-top">
-         <a href="#"><?php echo HTML::image($CLICSHOPPING_Template->getImageDirectory() . '/footer/top.gif', 'Retour en haut de la page', '16', '16'); ?></a></span>
-      </span>
+            <span class="col-md-1 navbar-text float-md-right text-md-right go-top"><a
+                href="#"><?php echo HTML::image($CLICSHOPPING_Template->getImageDirectory() . '/footer/top.gif', 'Retour en haut de la page', '16', '16'); ?></a></span>
+         </span>
     <?php
   }
 ?>
-     </div>
+
+            </div>
+          </span>
     </div>
-   </nav>
   </div>
   <div class="footerCadre separator"></div>
 </footer>
