513cec7706a9a7fc5bcff5394a02044d1289886c
ClicShopping <Yae8mr2o*>
2019-11-27 18:03:01 -0500

    version 3.2112 admin optimization

---
+++ shop/includes/Module/Hooks/ClicShoppingAdmin/Footer/FooterOutputLozad.php
@@ -0,0 +1,30 @@
+<?php
+  /**
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @licence MIT - Portion of osCommerce 2.4
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
+
+  namespace ClicShopping\OM\Module\Hooks\ClicShoppingAdmin\Footer;
+
+  class FooterOutputLozad
+  {
+    /**
+     * @return string
+     */
+    public function display(): string
+    {
+      $output = '<!--Lazyload Script start-->' . "\n";
+      $output .= '<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script>' . "\n";
+      $output .= '<script>';
+      $output .= 'const observer = lozad(); observer.observe();';
+      $output .= '</script>' . "\n";
+      $output .= '<!--End Lazyload Script-->' . "\n";
+
+      return $output;
+    }
+  }
