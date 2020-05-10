246d6ec21ddc89f13ef2ee3dd35f276dce6ff85f
ClicShopping <Yae8mr2o*>
2020-05-10 11:15:08 +0200

    fix error on foreach $tag

--- shop/sources/template/Default/modules/modules_front_page/fp_new_products.php
+++ shop/sources/template/Default/modules/modules_front_page/fp_new_products.php
@@ -275,7 +275,7 @@
                 $tag = $CLICSHOPPING_ProductsFunctionTemplate->getProductsHeadTag($products_id);
 
                 $products_tag = '';
-                if (!empty($tag)) {
+                if (is_array($tag) && !empty($tag)) {
                   foreach ($tag as $value) {
                     $products_tag .= '#<span class="productTag">' . HTML::link(CLICSHOPPING::link(null, 'Search&keywords='. HTML::outputProtected(utf8_decode($value) .'&search_in_description=1&categories_id=&inc_subcat=1'), 'rel="nofollow"'), $value) . '</span> ';
                   }
