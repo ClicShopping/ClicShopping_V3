e1f53c53770a08744e897cc15bb61335340760b6
ClicShopping <Yae8mr2o*>
2020-05-23 23:07:34 +0200

    update bootstrap

--- shop/includes/Module/Hooks/ClicShoppingAdmin/Footer/FooterOutputBootstrap.php
+++ shop/includes/Module/Hooks/ClicShoppingAdmin/Footer/FooterOutputBootstrap.php
@@ -21,7 +21,7 @@
 
 //Note : Could be relation with a meta tag allowing to implement a new boostrap theme : Must be installed
       $output = '<! -- Start BootStrap -->';
-      $output .= '<script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>' . "\n";
+      $output .= '<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>' . "\n";
       $output .= '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>' . "\n";
 
       $output .= '<!-- End bootstrap  -->' . "\n";
