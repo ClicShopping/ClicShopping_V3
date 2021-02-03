328befc1307ca23c59a3d0682ee21d15dcd0e1a9
ClicShopping <Yae8mr2o*>
2021-02-03 11:42:52 +0100

    opcode optimization

--- ClicShoppingAdmin/ajax/map.php
+++ ClicShoppingAdmin/ajax/map.php
@@ -45,9 +45,9 @@
 
   if (is_array($results)) {
     foreach ($results as $result) {
-      $json[strtolower($result['countries_iso_code_2'])] = ['total' => $result['total'],
+      $json[strtolower($result['countries_iso_code_2'])] = [
+        'total' => $result['total'],
         'amount' => $result['amount'],
-
       ];
     }
   }
