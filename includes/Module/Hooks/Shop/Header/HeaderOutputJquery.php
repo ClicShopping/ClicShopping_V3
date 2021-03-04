34ddb4a35c7adde6769ac2e02626bfd3827121db
ClicShopping <Yae8mr2o*>
2021-03-04 12:24:46 +0100

    jquery 3.6

--- includes/Module/Hooks/Shop/Header/HeaderOutputJquery.php
+++ includes/Module/Hooks/Shop/Header/HeaderOutputJquery.php
@@ -18,7 +18,7 @@
      */
     public function display(): string
     {
-     $output = '<script src="https://code.jquery.com/jquery-3.5.1.min.js"  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>' . "\n";
+     $output = '<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>' . "\n";
 
      return $output;
     }
--- install/templates/main_page.php
+++ install/templates/main_page.php
@@ -33,8 +33,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   <link rel="stylesheet preload" as="style" href="../ext/javascript/bootstrap/css/bootstrap_icons_1.2.1.css" media="screen, print">
 
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <meta name="robots" content="noindex,nofollow">
 
   <link rel="stylesheet" href="templates/main_page/stylesheet.css">
