2aa663286514cbaf76e141d8903b3bc15077e6da
ClicShopping <Yae8mr2o*>
2021-03-06 12:09:21 +0100

    little fix

--- includes/Module/Hooks/Shop/Header/HeaderOutputBootstrap.php
+++ includes/Module/Hooks/Shop/Header/HeaderOutputBootstrap.php
@@ -27,8 +27,8 @@
       if (!\defined('MODULE_HEADER_TAGS_BOOTSTRAP_SELECT_THEME') || MODULE_HEADER_TAGS_BOOTSTRAP_SELECT_THEME == 'False') {
         $output = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">' . "\n";
         $output .= '<link rel="stylesheet" media="screen, print" href="' . $CLICSHOPPING_Template->getTemplateCSS() . '" />' . "\n";
-  
-        $output .= '<link rel="stylesheet" href="' . HTTP::getShopUrlDomain() . 'ext/javascript/bootstrap/css/bootstrap_icons_1.2.1.css'  . '" media="screen, print">';
+        $output .= '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">';
+
         return $output;
       } else {
         return false;
