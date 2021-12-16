92fa9bcd7bb4bb42da4efac85621abd63d2ba865
ClicShopping <admin@clicshopping.org>
2021-12-16 16:20:01 +0100

    little fix

--- includes/ClicShopping/Apps/Payment/MoneyOrder/Sites/ClicShoppingAdmin/Pages/Home/templates/configure.php
+++ includes/ClicShopping/Apps/Payment/MoneyOrder/Sites/ClicShoppingAdmin/Pages/Home/templates/configure.php
@@ -43,7 +43,7 @@
   </div>
   <div class="separator"></div>
   <div class="col-md-12 alert alert-warning" role="alert">
-    <span><?php echo $CLICSHOPPING_Chronopost->getDef('text_warning_group'); ?></span>
+    <span><?php echo $CLICSHOPPING_MoneyOrder->getDef('text_warning_group'); ?></span>
   </div>
 
   <?php
