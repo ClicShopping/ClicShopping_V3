521202a31ce5f368c1d986b54991755715edc29c
ClicShopping <admin@clicshopping.org>
2022-02-23 17:50:09 +0100

    refactoring

--- includes/ClicShopping/Apps/Communication/PageManager/Module/ClicShoppingAdmin/Config/ConfigAbstract.php
+++ includes/ClicShopping/Apps/Communication/PageManager/Module/ClicShoppingAdmin/Config/ConfigAbstract.php
@@ -52,7 +52,7 @@
 
         $cfg = new $class($this->code);
 
-        $this->app->saveCfgParam($key, $cfg->default, isset($cfg->title) ? $cfg->title : null, isset($cfg->description) ? $cfg->description : null, isset($cfg->set_func) ? $cfg->set_func : null);
+        $this->app->saveCfgParam($key, $cfg->default, $cfg->title ?? null, $cfg->description ?? null, $cfg->set_func ?? null);
       }
     }
 
@@ -108,7 +108,7 @@
 
 
         if (!\defined($key)) {
-          $this->app->saveCfgParam($key, $cfg->default, isset($cfg->title) ? $cfg->title : null, isset($cfg->description) ? $cfg->description : null, isset($cfg->set_func) ? $cfg->set_func : null);
+          $this->app->saveCfgParam($key, $cfg->default, $cfg->title ?? null, $cfg->description ?? null, $cfg->set_func ?? null);
         }
 
         if ($cfg->app_configured !== false) {
