a3874f8ffd7cbfcdcbcc7e3faa793e8c4c4f8f73
ClicShopping <loic.richard2@gmail.com>
2022-07-20 08:26:46 +0200

    bootstrap@5.2.0

--- error_documents/error_template.php
+++ error_documents/error_template.php
@@ -31,5 +31,6 @@
 </div>
 </body>
 <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
 </html>
--- includes/Module/Hooks/ClicShoppingAdmin/Footer/FooterOutputBootstrap.php
+++ includes/Module/Hooks/ClicShoppingAdmin/Footer/FooterOutputBootstrap.php
@@ -19,7 +19,8 @@
     public function display(): string
     {
       $output = '<!-- Start BootStrap -->' . "\n";
-      $output .= '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>';
+      $output .= '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+';
       $output .= '<!-- End bootstrap  -->' . "\n";
 
       return $output;
