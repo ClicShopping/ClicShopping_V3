3f8f194ec1e54b88fd7b5d6aa6b8e92f5e092698
ClicShopping <admin@clicshopping.org>
2022-08-18 18:13:29 -0400

    update bootstrap icon version

--- includes/Module/Hooks/ClicShoppingAdmin/Header/HeaderOutputBootstrap.php
+++ includes/Module/Hooks/ClicShoppingAdmin/Header/HeaderOutputBootstrap.php
@@ -24,7 +24,7 @@
       $output = '<!-- Start Bootstrap -->' . "\n";
       $output .= '<!-- CSS only -->';
       $output .= '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">' . "\n";
-      $output .= '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">';
+      $output .= '<link rel="stylesheet" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">';
       $output .= '<link rel="stylesheet" href="' . CLICSHOPPING::link('css/bootstrap_icons_customize.css')  . '" media="screen, print">';
       $output .= '<!-- Start Bootstrap -->' . "\n";
       
