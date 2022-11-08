262ee2c49a5f9b889bee78b5e9ac7bb4cdf6421c
ClicShopping <admin@clicshopping.org>
2022-11-08 10:12:30 -0500

    fix js error

--- ext/javascript/clicshopping/ClicShoppingAdmin/general.js
+++ ext/javascript/clicshopping/ClicShoppingAdmin/general.js
@@ -10,45 +10,53 @@
 function SetFocus() {
   if (document.forms.length > 0) {
     isNotAdminLanguage:
-    for (f=0; f<document.forms.length; f++) {
-      if (document.forms[f].name != "adminlanguage") {
-        const field = document.forms[f];
-        for (i=0; i<field.length; i++) {
-          if ( (field.elements[i].type != "image") &&
-               (field.elements[i].type != "hidden") &&
-               (field.elements[i].type != "reset") &&
-               (field.elements[i].type != "button") &&
-               (field.elements[i].type != "submit") &&
-               (field.elements[i].disabled != true)) {
+      for (f=0; f<document.forms.length; f++) {
+        if (document.forms[f].name != "adminlanguage") {
+          const field = document.forms[f];
+          for (i=0; i<field.length; i++) {
+            if ( (field.elements[i].type != "image") &&
+              (field.elements[i].type != "hidden") &&
+              (field.elements[i].type != "reset") &&
+              (field.elements[i].type != "button") &&
+              (field.elements[i].type != "submit") &&
+              (field.elements[i].disabled != true)
+            ) {
 
-            document.forms[f].elements[i].focus();
+              document.forms[f].elements[i].focus();
 
-            if ( (field.elements[i].type == "text") ||
-                 (field.elements[i].type == "password") )
-              document.forms[f].elements[i].select();
+              if ( (field.elements[i].type == "text") ||
+                (field.elements[i].type == "password")
+              )
+                document.forms[f].elements[i].select();
 
-            break isNotAdminLanguage;
+              break isNotAdminLanguage;
+            }
           }
         }
       }
-    }
   }
 }
 
 function toggleDivBlock(id) {
+  let itm;
+
   if (document.getElementById) {
     itm = document.getElementById(id);
-  } elseif (document.all){
+  }
+
+  if (document.all) {
     itm = document.all[id];
-  } elseif (document.layers){
+  }
+
+  if (document.layers){
     itm = document.layers[id];
   }
 
   if (itm) {
-    if (itm.style.display != "none") {
-      itm.style.display = "none";
+    if (itm.style.display != 'none') {
+      itm.style.display = 'none';
     } else {
-      itm.style.display = "block";
+      itm.style.display = 'block';
     }
   }
 }
