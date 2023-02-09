7e07135746926410cbc199bef2783fb19f67c8b5
ClicShopping <loic.richard2@gmail.com>
2023-02-09 10:52:38 -0500

    update

--- includes/ClicShopping/External/vendor/composer/installed.php
+++ includes/ClicShopping/External/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '9625eb3722bc2866c78ba601ee53359fcb31102f',
+        'reference' => '16288496f38b6c6d16aa622a46d3e655eb767d64',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../../../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '9625eb3722bc2866c78ba601ee53359fcb31102f',
+            'reference' => '16288496f38b6c6d16aa622a46d3e655eb767d64',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../../../../',
             'aliases' => array(),
