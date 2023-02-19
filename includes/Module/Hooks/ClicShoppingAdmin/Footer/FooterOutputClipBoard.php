7adff7d4709ca371d55c3e641fee2e22f03cbf00
ClicShopping <loic.richard2@gmail.com>
2023-02-19 11:47:55 -0500

    fix call clipboard

--- includes/Module/Hooks/ClicShoppingAdmin/Footer/FooterOutputClipBoard.php
+++ includes/Module/Hooks/ClicShoppingAdmin/Footer/FooterOutputClipBoard.php
@@ -17,14 +17,14 @@
      */
     public function display(): string
     {
-     $output = '';
-     
+      $output = '';
+
       if (isset($_SESSION['admin'])) {
         $output .= '<!-- Start Clipboard -->' . "\n";
-        $output .= '<script defer src="https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js"></script>';
+        $output .= '<script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js"></script>' . "\n";
         $output .= '<!-- End Clipboard  -->' . "\n";
       }
-      
+
       return $output;
     }
   }
