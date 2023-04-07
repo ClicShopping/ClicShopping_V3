c205543576af24cf9e418ef6e9c60d4e7b288c89
ClicShopping <loic.richard2@gmail.com>
2023-04-07 12:44:18 -0400

    fix js error

--- sources/template/Default/modules/modules_account_customers/content/account_customers_password.php
+++ sources/template/Default/modules/modules_account_customers/content/account_customers_password.php
@@ -38,7 +38,7 @@
         <label for="inputPasswordNew" class="col-4 col-form-label"><?php echo CLICSHOPPING::getDef('entry_password_new'); ?></label>
         <div class="col-md-8">
           <div class="btn-group" role="group" aria-label="buttonGroup">
-           <span><?php echo HTML::inputField('password_new', null, 'required aria-required="true" autocomplete="off" id="inputPassword" aria-describedby="' . CLICSHOPPING::getDef('entry_password_new') . '" placeholder="' . CLICSHOPPING::getDef('entry_password_new') . '"  minlength="' . (int)ENTRY_PASSWORD_MIN_LENGTH . '"'); ?></span>
+           <span><?php echo HTML::inputField('password_new', null, 'required aria-required="true" autocomplete="off" id="input-password" aria-describedby="' . CLICSHOPPING::getDef('entry_password_new') . '" placeholder="' . CLICSHOPPING::getDef('entry_password_new') . '"  minlength="' . (int)ENTRY_PASSWORD_MIN_LENGTH . '"'); ?></span>
            <span><button type="button" id="button-generate" class="btn btn-primary btn-sm"><i class="bi bi-arrow-clockwise"></i></button></span>
           <?php
             if (!\is_null(CLICSHOPPING::getDef('entry_password_current_text'))) echo '<span class="form-text">' . CLICSHOPPING::getDef('entry_password_current_text') . '</span>';
