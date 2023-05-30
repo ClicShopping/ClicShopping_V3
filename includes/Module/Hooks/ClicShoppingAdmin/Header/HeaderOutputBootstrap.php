6bfde0f2e79d70a0ef8d9f943786747166ea4339
ClicShopping <loic.richard2@gmail.com>
2023-05-30 10:16:23 -0400

    bootstrap-icons@1.10.3

--- includes/Module/Hooks/ClicShoppingAdmin/Header/HeaderOutputBootstrap.php
+++ includes/Module/Hooks/ClicShoppingAdmin/Header/HeaderOutputBootstrap.php
@@ -25,7 +25,7 @@
       $output .= '<!-- CSS only -->';
       $output .= '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 ' . "\n";
-      $output .= '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">';
+      $output .= '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">';
       $output .= '<link rel="stylesheet" href="' . CLICSHOPPING::link('css/bootstrap_icons_customize.css')  . '" media="screen, print">';
       $output .= '<!-- Start Bootstrap -->' . "\n";
       
--- install/templates/main_page.php
+++ install/templates/main_page.php
@@ -30,7 +30,7 @@
   <title>ClicShopping, Social E-Commerce B2B/B2C Open Source Solutions</title>
   <link rel="shortcut icon" href="../images/favicon.png" type="image/x-icon" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <link rel="stylesheet" href="templates/main_page/stylesheet.css">
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
