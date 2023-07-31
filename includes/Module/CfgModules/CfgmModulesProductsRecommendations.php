dad5caf48bb1ea41976500372a932a173e671e4b
ClicShopping <loic.richard2@gmail.com>
2023-07-31 14:35:26 -0400

    update recommendations

---
+++ includes/Module/CfgModules/CfgmModulesProductsRecommendations.php
@@ -0,0 +1,34 @@
+<?php
+  /**
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
+
+  use ClicShopping\OM\Registry;
+  use ClicShopping\OM\CLICSHOPPING;
+
+  class CfgmModulesProductsRecommendations
+  {
+    public string $code = 'modules_products_recommendations';
+    public string $directory;
+    public $language_directory;
+    public string $site = 'Shop';
+    public string $key = 'MODULE_MODULES_PRODUCTS_RECOMMENDATIONS_INSTALLED';
+    public $title;
+    public bool $template_integration = true;
+
+    public function __construct()
+    {
+      $CLICSHOPPING_Template = Registry::get('TemplateAdmin');
+
+      $this->directory = $CLICSHOPPING_Template->getDirectoryPathShopDefaultTemplateHtml() . '/modules/modules_products_recommendations/';
+      $this->language_directory = $CLICSHOPPING_Template->getPathLanguageShopDirectory();
+
+      $this->title = CLICSHOPPING::getDef('module_cfg_module_products_recommendations_modules_title');
+    }
+  }
+
