658ade6b86d96b3fb4bd8c5cbb806791a6ebb5d4
ClicShopping <loic.richard2@gmail.com>
2023-09-19 15:35:12 -0400

    add missing  element

---
+++ includes/ClicShopping/Apps/Customers/Reviews/Module/ClicShoppingAdmin/Config/RV/Params/review_number.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * @copyright 2008 - https://www.clicshopping.org
+ * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ * @Licence GPL 2 & MIT
+ * @Info : https://www.clicshopping.org/forum/trademark/
+ *
+ */
+
+namespace ClicShopping\Apps\Customers\Reviews\Module\ClicShoppingAdmin\Config\RV\Params;
+
+class review_number extends \ClicShopping\Apps\Customers\Reviews\Module\ClicShoppingAdmin\Config\ConfigParamAbstract
+{
+
+  public $default = '10';
+  public ?int $sort_order = 30;
+  public bool $app_configured = true;
+
+  protected function init()
+  {
+    $this->title = $this->app->getDef('cfg_reviews_review_number_title');
+    $this->description = $this->app->getDef('cfg_reviews_review_number_description');
+  }
+}
