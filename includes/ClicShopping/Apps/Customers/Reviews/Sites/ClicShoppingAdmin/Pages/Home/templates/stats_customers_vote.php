a58751c90d477118e1ac284d634bcdf2f5a75cde
ClicShopping <loic.richard2@gmail.com>
2023-09-28 17:22:22 -0400

    add new report on vote reviews

--- includes/ClicShopping/Apps/Customers/Reviews/Sites/ClicShoppingAdmin/Pages/Home/templates/stats_customers_vote.php
+++ includes/ClicShopping/Apps/Customers/Reviews/Sites/ClicShoppingAdmin/Pages/Home/templates/stats_customers_vote.php
@@ -26,13 +26,24 @@ if ($CLICSHOPPING_MessageStack->exists('reviews')) {
 $page = (isset($_GET['page']) && is_numeric($_GET['page'])) ? (int)$_GET['page'] : 1;
 
 $QTotal = $CLICSHOPPING_Reviews->db->prepare('select id as count,
-                                                     SUM(CASE WHEN vote = 1 THEN 1 ELSE 0 END) AS vote_yes,
-                                                     SUM(CASE WHEN vote = 0 THEN 1 ELSE 0 END) AS vote_no,
-                                                     SUM(CASE WHEN vote IN (0, 1) THEN 1 ELSE 0 END) AS total_votes
-                                              from :table_reviews_vote
+                                                    SUM(CASE WHEN vote = 1 THEN 1 ELSE 0 END) AS vote_yes,
+                                                    SUM(CASE WHEN vote = 0 THEN 1 ELSE 0 END) AS vote_no,
+                                                    SUM(CASE WHEN vote IN (0, 1) THEN 1 ELSE 0 END) AS total_votes
+                                             from :table_reviews_vote
+                                             where reviews_id <> 0
                                             ');
 
 $QTotal->execute();
+
+$QTotalIa = $CLICSHOPPING_Reviews->db->prepare('select id as count_ia,
+                                                      SUM(CASE WHEN sentiment = 1 THEN 1 ELSE 0 END) AS vote_ia_yes,
+                                                      SUM(CASE WHEN sentiment = 0 THEN 1 ELSE 0 END) AS vote_ia_no,
+                                                      SUM(CASE WHEN sentiment IN (0, 1) THEN 1 ELSE 0 END) AS total_ia_votes
+                                              from :table_reviews_vote
+                                              where reviews_id = 0
+                                            ');
+
+$QTotalIa->execute();
 ?>
 <div class="contentBody">
   <div class="row">
@@ -72,12 +83,42 @@ $QTotal->execute();
                         <div
                           class="col-sm-12 text-white"><?php echo $CLICSHOPPING_Reviews->getDef('text_count_vote_yes') . ' ' . $count_vote_yes . ' - ' . round($percentage_yes, 2) . '%'; ?></div>
                         <div
-                          class="col-sm-12 text-white"><?php echo $CLICSHOPPING_Reviews->getDef('text_average_no') . ' ' . $count_vote_no . ' - ' . round($percentage_no, 2) . '%'; ?></div>
+                          class="col-sm-12 text-white"><?php echo $CLICSHOPPING_Reviews->getDef('text_count_vote_no') . ' ' . $count_vote_no . ' - ' . round($percentage_no, 2) . '%'; ?></div>
                       </span>
                     </div>
                   </div>
                 </div>
               </div>
+              <div class="separator"></div>
+            </div>
+            <?php
+          }
+
+          if ($QTotalIa->valueInt('count_ia') > 0) {
+            $count_ia_yes = $QTotalIa->valueInt('vote_ia_yes');
+            $count_ia_no = $QTotalIa->valueInt('vote_ia_no');
+            $percentage_ia_yes =  ($QTotalIa->valueInt('vote_ia_yes') / $QTotalIa->valueInt('total_ia_votes')) * 100;
+            $percentage_ia_no = ($QTotalIa->valueInt('vote_ia_no') / $QTotalIa->valueInt('total_ia_votes')) * 100;
+          ?>
+            <div class="col-md-2 col-12">
+              <div class="card bg-success">
+                <div class="card-body">
+                  <h6 class="card-title text-white"><i
+                            class="bi bi-bar-chart text-white"></i> <?php echo '&nbsp;' . $CLICSHOPPING_Reviews->getDef('text_statistics'); ?>
+                  </h6>
+                  <div class="card-text">
+                    <div class="col-sm-12">
+                      <span class="float-end">
+                        <div
+                                class="col-sm-12 text-white"><?php echo $CLICSHOPPING_Reviews->getDef('text_count_vote_yes') . ' ' . $count_ia_yes . ' - ' . round($percentage_ia_yes, 2) . '%'; ?></div>
+                        <div
+                                class="col-sm-12 text-white"><?php echo $CLICSHOPPING_Reviews->getDef('text_count_vote_no') . ' ' . $count_ia_no . ' - ' . round($percentage_ia_no, 2) . '%'; ?></div>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="separator"></div>
             </div>
             <?php
           }
@@ -132,18 +173,31 @@ $QTotal->execute();
           data-sortable="true" class="text-center"><?php echo $CLICSHOPPING_Reviews->getDef('table_heading_percentage_yes'); ?></th>
       <th data-field="percentNo"
           data-sortable="true" class="text-center"><?php echo $CLICSHOPPING_Reviews->getDef('table_heading_percentage_no'); ?></th>
+
+      <th data-field="IaYes"
+          data-sortable="true" class="text-center"><?php echo $CLICSHOPPING_Reviews->getDef('table_heading_ai_vote_yes'); ?></th>
+      <th data-field="YaNo"
+          data-sortable="true" class="text-center"><?php echo $CLICSHOPPING_Reviews->getDef('table_heading_ai_vote_no'); ?></th>
+      <th data-field="percentIaYes"
+          data-sortable="true" class="text-center"><?php echo $CLICSHOPPING_Reviews->getDef('table_heading_percentage_ai_yes'); ?></th>
+      <th data-field="percentIaNo"
+          data-sortable="true" class="text-center"><?php echo $CLICSHOPPING_Reviews->getDef('table_heading_percentage_ai_no'); ?></th>
+
+
     </tr>
     </thead>
     <tbody>
     <?php
       $QReport = $CLICSHOPPING_Reviews->db->prepare('select distinct SQL_CALC_FOUND_ROWS  r.id,
-                                                                                         r.products_id,
-                                                                                         r.reviews_id,
-                                                                                         SUM(CASE WHEN r.vote = 1 THEN 1 ELSE 0 END) AS vote_yes,
-                                                                                         SUM(CASE WHEN r.vote = 0 THEN 1 ELSE 0 END) AS vote_no,
-                                                                                         SUM(CASE WHEN r.vote IN (0, 1) THEN 1 ELSE 0 END) AS total_votes,
-                                                                                         r.sentiment,
-                                                                                         p.products_image
+                                                                                          r.products_id,
+                                                                                          r.reviews_id,
+                                                                                          SUM(CASE WHEN r.vote = 1 AND r.reviews_id <> 0 THEN 1 ELSE 0 END) AS vote_yes,
+                                                                                          SUM(CASE WHEN r.vote = 0 AND r.reviews_id <> 0 THEN 1 ELSE 0 END) AS vote_no,
+                                                                                          SUM(CASE WHEN r.vote IN (0, 1) AND r.reviews_id <> 0 THEN 1 ELSE 0 END) AS total_votes,
+                                                                                          SUM(CASE WHEN r.sentiment = 1 and r.reviews_id = 0 THEN 1 ELSE 0 END) AS ia_yes,
+                                                                                          SUM(CASE WHEN r.sentiment = 0 and r.reviews_id = 0 THEN 1 ELSE 0 END) AS ia_no,
+                                                                                          SUM(CASE WHEN r.sentiment IN (0, 1) and r.reviews_id = 0 THEN 1 ELSE 0 END) AS total_ai_votes,
+                                                                                          p.products_image
                                                       from :table_reviews_vote r,
                                                            :table_products p
                                                       where r.products_id = p.products_id
@@ -163,6 +217,11 @@ $QTotal->execute();
             $count_vote_no = $QReport->valueInt('vote_no');
             $percentage_yes =  ($QReport->valueInt('vote_yes') /  $QReport->valueInt('total_votes')) * 100;
             $percentage_no = ($QReport->valueInt('vote_no') /  $QReport->valueInt('total_votes')) * 100;
+
+            $count_ai_yes = $QReport->valueInt('ia_yes');
+            $count_ai_no = $QReport->valueInt('ia_no');
+            $percentage_ai_yes =  ($QReport->valueInt('ia_yes') / $QReport->valueInt('total_ai_votes')) * 100;
+            $percentage_ai_no = ($QReport->valueInt('ia_no') /  $QReport->valueInt('total_ai_votes')) * 100;
         ?>
             <td></td>
             <td><?php echo $QReport->valueInt('products_id'); ?></td>
@@ -174,6 +233,10 @@ $QTotal->execute();
             <td><?php echo $count_vote_no;?></td>
             <td><?php echo round($percentage_yes, 2);?> %</td>
             <td><?php echo round($percentage_no, 2);?> %</td>
+            <td><?php echo $count_ai_yes;?></td>
+            <td><?php echo $count_ai_no;?></td>
+            <td><?php echo round($percentage_ai_yes, 2);?> %</td>
+            <td><?php echo round($percentage_ai_no, 2);?> %</td>
           </tr>
           <?php
         }
