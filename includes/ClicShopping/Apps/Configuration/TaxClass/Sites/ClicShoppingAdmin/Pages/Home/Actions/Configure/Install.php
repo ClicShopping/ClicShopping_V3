94dd92700ccd4b1e3a6e1022168843f0e1b4034a
ClicShopping <loic.richard2@gmail.com>
2023-09-29 16:04:30 -0400

    Refactoring add Db directoy

--- includes/ClicShopping/Apps/Configuration/TaxClass/Sites/ClicShoppingAdmin/Pages/Home/Actions/Configure/Install.php
+++ includes/ClicShopping/Apps/Configuration/TaxClass/Sites/ClicShoppingAdmin/Pages/Home/Actions/Configure/Install.php
@@ -10,15 +10,13 @@
 
 namespace ClicShopping\Apps\Configuration\TaxClass\Sites\ClicShoppingAdmin\Pages\Home\Actions\Configure;
 
-use ClicShopping\OM\Cache;
 use ClicShopping\OM\Registry;
+use ClicShopping\Apps\Configuration\TaxClass\Sql\MariaDb\MariaDb;
 
 class Install extends \ClicShopping\OM\PagesActionsAbstract
 {
-
   public function execute()
   {
-
     $CLICSHOPPING_MessageStack = Registry::get('MessageStack');
     $CLICSHOPPING_TaxClass = Registry::get('TaxClass');
 
@@ -29,82 +27,13 @@ class Install extends \ClicShopping\OM\PagesActionsAbstract
     $m = Registry::get('TaxClassAdminConfig' . $current_module);
     $m->install();
 
-    static::installDbMenuAdministration();
-    static::installProductsTaxClassDb();
+    //add condition to select mariaDb ou postgres
+    Registry::set('MariaDb', new MariaDb());
+    $CLICSHOPPING_MariaDb = Registry::get('MariaDb');
+    $CLICSHOPPING_MariaDb->execute();
 
     $CLICSHOPPING_MessageStack->add($CLICSHOPPING_TaxClass->getDef('alert_module_install_success'), 'success');
 
     $CLICSHOPPING_TaxClass->redirect('Configure&module=' . $current_module);
-  }
-
-  private static function installDbMenuAdministration(): void
-  {
-    $CLICSHOPPING_Db = Registry::get('Db');
-    $CLICSHOPPING_TaxClass = Registry::get('TaxClass');
-    $CLICSHOPPING_Language = Registry::get('Language');
-
-    $Qcheck = $CLICSHOPPING_Db->get('administrator_menu', 'app_code', ['app_code' => 'app_configuration_tax_class']);
-
-    if ($Qcheck->fetch() === false) {
-
-      $sql_data_array = ['sort_order' => 2,
-        'link' => 'index.php?A&Configuration\TaxClass&TaxClass',
-        'image' => 'tax_classes.gif',
-        'b2b_menu' => 0,
-        'access' => 0,
-        'app_code' => 'app_configuration_tax_class'
-      ];
-
-      $insert_sql_data = ['parent_id' => 19];
-
-      $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
-
-      $CLICSHOPPING_Db->save('administrator_menu', $sql_data_array);
-
-      $id = $CLICSHOPPING_Db->lastInsertId();
-
-      $languages = $CLICSHOPPING_Language->getLanguages();
-
-      for ($i = 0, $n = \count($languages); $i < $n; $i++) {
-
-        $language_id = $languages[$i]['id'];
-
-        $sql_data_array = ['label' => $CLICSHOPPING_TaxClass->getDef('title_menu')];
-
-        $insert_sql_data = [
-          'id' => (int)$id,
-          'language_id' => (int)$language_id
-        ];
-
-        $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
-
-        $CLICSHOPPING_Db->save('administrator_menu_description', $sql_data_array);
-
-      }
-
-      Cache::clear('menu-administrator');
-    }
-  }
-
-
-  private static function installProductsTaxClassDb()
-  {
-    $CLICSHOPPING_Db = Registry::get('Db');
-
-    $Qcheck = $CLICSHOPPING_Db->query('show tables like ":table_tax_class"');
-
-    if ($Qcheck->fetch() === false) {
-      $sql = <<<EOD
-CREATE TABLE :table_tax_class (
-  tax_class_id int NOT NULL auto_increment,
-  tax_class_title varchar(32) NOT NULL,
-  tax_class_description varchar(255) NOT NULL,
-  last_modified datetime,
-  date_added datetime NOT NULL
-  PRIMARY KEY tax_class_id
-) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-EOD;
-      $CLICSHOPPING_Db->exec($sql);
-    }
   }
 }
--- includes/ClicShopping/Apps/Configuration/TaxRates/Sites/ClicShoppingAdmin/Pages/Home/Actions/Configure/Install.php
+++ includes/ClicShopping/Apps/Configuration/TaxRates/Sites/ClicShoppingAdmin/Pages/Home/Actions/Configure/Install.php
@@ -10,15 +10,13 @@
 
 namespace ClicShopping\Apps\Configuration\TaxRates\Sites\ClicShoppingAdmin\Pages\Home\Actions\Configure;
 
-use ClicShopping\OM\Cache;
 use ClicShopping\OM\Registry;
+use ClicShopping\Apps\Configuration\TaxRates\Sql\MariaDb\MariaDb;
 
 class Install extends \ClicShopping\OM\PagesActionsAbstract
 {
-
   public function execute()
   {
-
     $CLICSHOPPING_MessageStack = Registry::get('MessageStack');
     $CLICSHOPPING_TaxRates = Registry::get('TaxRates');
 
@@ -29,85 +27,13 @@ class Install extends \ClicShopping\OM\PagesActionsAbstract
     $m = Registry::get('TaxRatesAdminConfig' . $current_module);
     $m->install();
 
-    static::installDbMenuAdministration();
-    static::installProductsTaxRatesDb();
+    //add condition to select mariaDb ou postgres
+    Registry::set('MariaDb', new MariaDb());
+    $CLICSHOPPING_MariaDb = Registry::get('MariaDb');
+    $CLICSHOPPING_MariaDb->execute();;
 
     $CLICSHOPPING_MessageStack->add($CLICSHOPPING_TaxRates->getDef('alert_module_install_success'), 'success', 'TaxRates');
 
     $CLICSHOPPING_TaxRates->redirect('Configure&module=' . $current_module);
-  }
-
-  private static function installDbMenuAdministration(): void
-  {
-    $CLICSHOPPING_Db = Registry::get('Db');
-    $CLICSHOPPING_TaxRates = Registry::get('TaxRates');
-    $CLICSHOPPING_Language = Registry::get('Language');
-
-    $Qcheck = $CLICSHOPPING_Db->get('administrator_menu', 'app_code', ['app_code' => 'app_configuration_tax_rates']);
-
-    if ($Qcheck->fetch() === false) {
-
-      $sql_data_array = ['sort_order' => 4,
-        'link' => 'index.php?A&Configuration\TaxRates&TaxRates',
-        'image' => 'tax_rates.gif',
-        'b2b_menu' => 0,
-        'access' => 0,
-        'app_code' => 'app_configuration_tax_rates'
-      ];
-
-      $insert_sql_data = ['parent_id' => 19];
-
-      $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
-
-      $CLICSHOPPING_Db->save('administrator_menu', $sql_data_array);
-
-      $id = $CLICSHOPPING_Db->lastInsertId();
-
-      $languages = $CLICSHOPPING_Language->getLanguages();
-
-      for ($i = 0, $n = \count($languages); $i < $n; $i++) {
-
-        $language_id = $languages[$i]['id'];
-
-        $sql_data_array = ['label' => $CLICSHOPPING_TaxRates->getDef('title_menu')];
-
-        $insert_sql_data = [
-          'id' => (int)$id,
-          'language_id' => (int)$language_id
-        ];
-
-        $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
-
-        $CLICSHOPPING_Db->save('administrator_menu_description', $sql_data_array);
-
-      }
-
-      Cache::clear('menu-administrator');
-    }
-  }
-
-  private static function installProductsTaxRatesDb()
-  {
-    $CLICSHOPPING_Db = Registry::get('Db');
-
-    $Qcheck = $CLICSHOPPING_Db->query('show tables like ":table_tax_rates"');
-
-    if ($Qcheck->fetch() === false) {
-      $sql = <<<EOD
-CREATE TABLE :table_tax_rates (
-  tax_rates_id int NOT NULL auto_increment,
-  tax_zone_id int NOT NULL,
-  tax_class_id int NOT NULL,
-  tax_priority int(5) default(1),
-  tax_rate decimal(7,4) NOT NULL,
-  tax_description varchar(255) NOT NULL,
-  last_modified datetime,
-  date_added datetime NOT NULL,
-  code_tax_erp varchar(15) null
-  PRIMARY KEY tax_rates_id
-) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-EOD;
-      $CLICSHOPPING_Db->exec($sql);
-    }
   }
 }
