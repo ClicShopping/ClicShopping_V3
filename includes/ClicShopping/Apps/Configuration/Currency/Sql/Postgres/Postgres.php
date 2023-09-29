e685980f5ea6604a0f7b669f1e8f5d6edb143d6d
ClicShopping <loic.richard2@gmail.com>
2023-09-29 14:43:36 -0400

    Refactoring add Db directoy

---
+++ includes/ClicShopping/Apps/Configuration/Currency/Sql/Postgres/Postgres.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * @copyright 2008 - https://www.clicshopping.org
+ * @Brand : ClicShoppingAI(TM) at Inpi all right Reserved
+ * @Licence GPL 2 & MIT
+ * @Info : https://www.clicshopping.org/forum/trademark/
+ *
+ */
+
+namespace ClicShopping\Apps\Configuration\Currency\Sql\MariaDb;
+
+use ClicShopping\OM\Cache;
+use ClicShopping\OM\Registry;
+
+class Postgres
+{
+  public function execute()
+  {
+    $CLICSHOPPING_Currency = Registry::get('Currency');
+    $CLICSHOPPING_Currency->loadDefinitions('Sites/ClicShoppingAdmin/install');
+
+    self::installDbMenuAdministration();
+    self::installDb();
+  }
+
+  /**
+   * @return void
+   */
+  private static function installDbMenuAdministration(): void
+  {
+    $CLICSHOPPING_Db = Registry::get('Db');
+    $CLICSHOPPING_Currency = Registry::get('Currency');
+    $CLICSHOPPING_Language = Registry::get('Language');
+
+    $Qcheck = $CLICSHOPPING_Db->get('administrator_menu', 'app_code', ['app_code' => 'app_configuration_currency']);
+
+    if ($Qcheck->fetch() === false) {
+      $sql_data_array = [
+        'sort_order' => 1,
+        'link' => 'index.php?A&Configuration\Currency&Currency',
+        'image' => 'currencies.gif',
+        'b2b_menu' => 0,
+        'access' => 0,
+        'app_code' => 'app_configuration_currency'
+      ];
+
+      $insert_sql_data = ['parent_id' => 20];
+      $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
+
+      $CLICSHOPPING_Db->save('administrator_menu', $sql_data_array);
+
+      $id = $CLICSHOPPING_Db->lastInsertId();
+      $languages = $CLICSHOPPING_Language->getLanguages();
+
+      for ($i = 0, $n = \count($languages); $i < $n; $i++) {
+        $language_id = $languages[$i]['id'];
+        $sql_data_array = ['label' => $CLICSHOPPING_Currency->getDef('title_menu')];
+
+        $insert_sql_data = [
+          'id' => (int)$id,
+          'language_id' => (int)$language_id
+        ];
+
+        $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
+
+        $CLICSHOPPING_Db->save('administrator_menu_description', $sql_data_array);
+
+      }
+
+      Cache::clear('menu-administrator');
+    }
+  }
+
+  /**
+   * @return void
+   */
+  private static function installCurrencyDb()
+  {
+    $CLICSHOPPING_Db = Registry::get('Db');
+
+    $Qcheck = $CLICSHOPPING_Db->query("SELECT to_regclass(':table_currencies')");
+
+    if ($Qcheck->fetch() === false) {
+      $sql = <<<EOD
+CREATE TABLE :table_currencies (
+  currencies_id serial PRIMARY KEY,
+  title varchar(255) NOT NULL,
+  code char(3) NOT NULL,
+  symbol_left varchar(12),
+  symbol_right varchar(12),
+  decimal_point char(1),
+  thousands_point char(1),
+  decimal_places char(1),
+  value numeric(13,8),
+  last_updated timestamp
+);
+EOD;
+      $CLICSHOPPING_Db->exec($sql);
+    }
+  }
+}
