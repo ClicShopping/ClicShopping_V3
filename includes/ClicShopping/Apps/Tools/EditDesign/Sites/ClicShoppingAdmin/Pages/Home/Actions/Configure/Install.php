83790c38a91d4d6c342aafaad81d4563246df1d8
ClicShopping <loic.richard2@gmail.com>
2023-10-01 09:05:06 -0400

    Refactoring add Db directoy

--- includes/ClicShopping/Apps/Tools/EditDesign/Sites/ClicShoppingAdmin/Pages/Home/Actions/Configure/Install.php
+++ includes/ClicShopping/Apps/Tools/EditDesign/Sites/ClicShoppingAdmin/Pages/Home/Actions/Configure/Install.php
@@ -10,8 +10,8 @@
 
 namespace ClicShopping\Apps\Tools\EditDesign\Sites\ClicShoppingAdmin\Pages\Home\Actions\Configure;
 
-use ClicShopping\OM\Cache;
 use ClicShopping\OM\Registry;
+use ClicShopping\Apps\Tools\EditDesign\Sql\MariaDb\MariaDb;
 
 class Install extends \ClicShopping\OM\PagesActionsAbstract
 {
@@ -27,59 +27,13 @@ class Install extends \ClicShopping\OM\PagesActionsAbstract
     $m = Registry::get('EditDesignAdminConfig' . $current_module);
     $m->install();
 
-    static::installDbMenuAdministration();
+//add condition to select mariaDb ou postgres
+    Registry::set('MariaDb', new MariaDb());
+    $CLICSHOPPING_MariaDb = Registry::get('MariaDb');
+    $CLICSHOPPING_MariaDb->execute();
 
     $CLICSHOPPING_MessageStack->add($CLICSHOPPING_EditDesign->getDef('alert_module_install_success'), 'success', 'EditDesign');
 
     $CLICSHOPPING_EditDesign->redirect('Configure&module=' . $current_module);
-  }
-
-  private static function installDbMenuAdministration(): void
-  {
-    $CLICSHOPPING_Db = Registry::get('Db');
-    $CLICSHOPPING_EditDesign = Registry::get('EditDesign');
-    $CLICSHOPPING_Language = Registry::get('Language');
-
-    $Qcheck = $CLICSHOPPING_Db->get('administrator_menu', 'app_code', ['app_code' => 'app_tools_design']);
-
-    if ($Qcheck->fetch() === false) {
-
-      $sql_data_array = [
-        'sort_order' => 3,
-        'link' => 'index.php?A&Tools\EditDesign&EditDesign',
-        'image' => '',
-        'b2b_menu' => 0,
-        'access' => 1,
-        'app_code' => 'app_tools_design'
-      ];
-
-      $insert_sql_data = ['parent_id' => 116];
-
-      $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
-
-      $CLICSHOPPING_Db->save('administrator_menu', $sql_data_array);
-
-      $id = $CLICSHOPPING_Db->lastInsertId();
-
-      $languages = $CLICSHOPPING_Language->getLanguages();
-
-      for ($i = 0, $n = \count($languages); $i < $n; $i++) {
-
-        $language_id = $languages[$i]['id'];
-
-        $sql_data_array = ['label' => $CLICSHOPPING_EditDesign->getDef('title_menu')];
-
-        $insert_sql_data = [
-          'id' => (int)$id,
-          'language_id' => (int)$language_id
-        ];
-
-        $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
-
-        $CLICSHOPPING_Db->save('administrator_menu_description', $sql_data_array);
-      }
-
-      Cache::clear('menu-administrator');
-    }
   }
 }
--- includes/ClicShopping/Apps/Tools/EditLogError/Sites/ClicShoppingAdmin/Pages/Home/Actions/Configure/Install.php
+++ includes/ClicShopping/Apps/Tools/EditLogError/Sites/ClicShoppingAdmin/Pages/Home/Actions/Configure/Install.php
@@ -10,8 +10,8 @@
 
 namespace ClicShopping\Apps\Tools\EditLogError\Sites\ClicShoppingAdmin\Pages\Home\Actions\Configure;
 
-use ClicShopping\OM\Cache;
 use ClicShopping\OM\Registry;
+use ClicShopping\Apps\Tools\EditLogError\Sql\MariaDb\MariaDb;
 
 class Install extends \ClicShopping\OM\PagesActionsAbstract
 {
@@ -27,91 +27,13 @@ class Install extends \ClicShopping\OM\PagesActionsAbstract
     $m = Registry::get('EditLogErrorAdminConfig' . $current_module);
     $m->install();
 
-    static::installDbMenuAdministration();
+//add condition to select mariaDb ou postgres
+    Registry::set('MariaDb', new MariaDb());
+    $CLICSHOPPING_MariaDb = Registry::get('MariaDb');
+    $CLICSHOPPING_MariaDb->execute();
 
     $CLICSHOPPING_MessageStack->add($CLICSHOPPING_EditLogError->getDef('alert_module_install_success'), 'success', 'EditLogError');
 
     $CLICSHOPPING_EditLogError->redirect('Configure&module=' . $current_module);
-  }
-
-  private static function installDbMenuAdministration(): void
-  {
-    $CLICSHOPPING_Db = Registry::get('Db');
-    $CLICSHOPPING_EditLogError = Registry::get('EditLogError');
-    $CLICSHOPPING_Language = Registry::get('Language');
-
-    $Qcheck = $CLICSHOPPING_Db->get('administrator_menu', 'app_code', ['app_code' => 'app_tools_edit_log_error']);
-
-    if ($Qcheck->fetch() === false) {
-      $sql_data_array = [
-        'sort_order' => 5,
-        'link' => 'index.php?A&Tools\EditLogError&LogError',
-        'image' => 'log.png',
-        'b2b_menu' => 0,
-        'access' => 1,
-        'app_code' => 'app_tools_edit_log_error'
-      ];
-
-      $insert_sql_data = ['parent_id' => 178];
-
-      $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
-
-      $CLICSHOPPING_Db->save('administrator_menu', $sql_data_array);
-
-      $id = $CLICSHOPPING_Db->lastInsertId();
-
-      $languages = $CLICSHOPPING_Language->getLanguages();
-
-      for ($i = 0, $n = \count($languages); $i < $n; $i++) {
-        $language_id = $languages[$i]['id'];
-
-        $sql_data_array = ['label' => $CLICSHOPPING_EditLogError->getDef('title_menu')];
-
-        $insert_sql_data = [
-          'id' => (int)$id,
-          'language_id' => (int)$language_id
-        ];
-
-        $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
-
-        $CLICSHOPPING_Db->save('administrator_menu_description', $sql_data_array);
-      }
-//menu 2
-      $sql_data_array = [
-        'sort_order' => 6,
-        'link' => 'index.php?A&Tools\EditLogError&LogErrorPhpMailer',
-        'image' => 'log.png',
-        'b2b_menu' => 0,
-        'access' => 1,
-        'app_code' => 'app_tools_edit_log_error'
-      ];
-
-      $insert_sql_data = ['parent_id' => 178];
-
-      $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
-
-      $CLICSHOPPING_Db->save('administrator_menu', $sql_data_array);
-
-      $id = $CLICSHOPPING_Db->lastInsertId();
-
-      $languages = $CLICSHOPPING_Language->getLanguages();
-
-      for ($i = 0, $n = \count($languages); $i < $n; $i++) {
-        $language_id = $languages[$i]['id'];
-
-        $sql_data_array = ['label' => $CLICSHOPPING_EditLogError->getDef('title_menu_phpmailer')];
-
-        $insert_sql_data = [
-          'id' => (int)$id,
-          'language_id' => (int)$language_id
-        ];
-
-        $sql_data_array = array_merge($sql_data_array, $insert_sql_data);
-
-        $CLICSHOPPING_Db->save('administrator_menu_description', $sql_data_array);
-      }
-
-      Cache::clear('menu-administrator');
-    }
   }
 }
