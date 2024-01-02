d65637d0b2ad101fa41d96d06df6c28360109092
ClicShopping <loic.richard2@gmail.com>
2024-01-02 10:28:54 -0500

    small update

---
+++ includes/ClicShopping/Sites/Shop/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * @copyright 2008 - https://www.clicshopping.org
+ * @Brand : ClicShoppingAI(TM) at Inpi all right Reserved
+ * @Licence GPL 2 & MIT
+ * @Info : https://www.clicshopping.org/forum/trademark/
+ *
+ */
+
+header('Location: /');
+exit;
+?>
