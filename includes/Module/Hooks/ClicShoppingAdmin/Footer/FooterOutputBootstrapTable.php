84472583006350ee656b72d1bcc93d1bd42fea42
ClicShopping <loic.richard2@gmail.com>
2024-06-30 17:39:00 -0400

    update librairies

--- includes/Module/Hooks/ClicShoppingAdmin/Footer/FooterOutputBootstrapTable.php
+++ includes/Module/Hooks/ClicShoppingAdmin/Footer/FooterOutputBootstrapTable.php
@@ -23,11 +23,11 @@ class FooterOutputBootstrapTable
 
     if (isset($_SESSION['admin'])) {
       $output .= '<!-- Start BootStrap Table -->' . "\n";
-      $output .= '<script defer src="https://unpkg.com/bootstrap-table@1.22.6/dist/bootstrap-table.min.js"></script>' . "\n";
+      $output .= '<script defer src="https://unpkg.com/bootstrap-table@1.23.0/dist/bootstrap-table.min.js"></script>' . "\n";
 //checkbox
       $output .= '<script defer src="' . CLICSHOPPING::link('Shop/ext/javascript/bootstrapTable/table_checkbox.js') . '"></script>' . "\n";
 //export
-      $output .= '<script defer src="https://unpkg.com/bootstrap-table@1.22.6/dist/extensions/export/bootstrap-table-export.min.js"></script>' . "\n";
+      $output .= '<script defer src="https://unpkg.com/bootstrap-table@1.23.0/dist/extensions/export/bootstrap-table-export.min.js"></script>' . "\n";
       $output .= '<script defer src="https://unpkg.com/tableexport.jquery.plugin/tableExport.min.js"></script>' . "\n";
 //mobile
       $output .= '<script defer src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.22.6/extensions/mobile/bootstrap-table-mobile.min.js"></script>' . "\n";
