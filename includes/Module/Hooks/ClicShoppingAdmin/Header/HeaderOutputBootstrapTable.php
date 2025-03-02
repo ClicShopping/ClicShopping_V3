5189f91b2d28b2f85669fe7460c3d28e11c6f1ab
ClicShopping <loic.richard2@gmail.com>
2025-03-02 08:51:35 -0500

    librairies updated

--- includes/Module/Hooks/ClicShoppingAdmin/Header/HeaderOutputBootstrapTable.php
+++ includes/Module/Hooks/ClicShoppingAdmin/Header/HeaderOutputBootstrapTable.php
@@ -23,7 +23,7 @@ class HeaderOutputBootstrapTable
 
     if (isset($_SESSION['admin'])) {
       $output = '<!-- Start BootStrap Table -->' . "\n";
-      $output .= '<link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.23.5/dist/bootstrap-table.min.css">' . "\n";
+      $output .= '<link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.24.1/dist/bootstrap-table.min.css">' . "\n";
       $output .= '<!-- Start BootStrap Table -->' . "\n";
     } else {
       return false;
